15581e7eb337b14bf962395a270befa21aab5e87
laurenwest12 <laurenwest12@gmail.com>
2021-07-23 15:25:46 -0400

    getting spreadsheet data from file location

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const express = require('express');
 const app = express();
 const firebase = require('firebase');
-const { GoogleSpreadsheet } = require('google-spreadsheet');
+const excelToJson = require('convert-excel-to-json');
 
 const { firebaseConfig, apiKey } = require('./config');
 firebase.initializeApp(firebaseConfig);
-
-const getSheet = async () => {
-	const doc = new GoogleSpreadsheet(
-		'1gaWIF1j7vsrGlKdZKCceIn1pwI1MlJEw-tFBsDBNluo'
-	);
-	await doc.useApiKey(apiKey);
-	await doc.loadInfo();
-	console.log(doc.title);
-};
 
 const db = firebase.firestore();
 // db.collection('Pong').doc('BlackPong1.0').set({
 // 	name: 'Black Pong 1.0',
 // });
 
+const result = excelToJson({
+	sourceFile: './spreadsheetData.xlsx',
+});
+
 app.listen(8080, async () => {
 	console.log('App is listening...');
-	await getSheet();
+	console.log(result);
 });
