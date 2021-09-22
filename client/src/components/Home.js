c4a54db0d385d007a8470b7a1b9c109c8958e2f4
laurenwest12 <laurenwest12@gmail.com>
2021-09-22 12:11:47 -0400

    Changed font and started home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,7 +4,23 @@ class Home extends Component {
 	render() {
 		return (
 			<div className="home">
-				<div className="home__section">What is Artist Pong?</div>
+				<div className="home__section">
+					<img
+						src="./pong.png"
+						alt="pong"
+						className="home__section__image"
+					/>
+					<div className="home__section__info">
+						<h1>What is Artist Pong?</h1>
+						<p>
+							Artist pong is a fun way to listen to your favorite
+							artists and also get drunk. It combines a playlist
+							that you control with common elements of beer pong.
+							The difference here is that you're all on the same
+							team.
+						</p>
+					</div>
+				</div>
 				<div className="home__section">How To</div>
 				<div className="home__section">Examples</div>
 			</div>
